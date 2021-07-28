e99676e3786bf0d098c9621cd9db0bf0217a021f
jeroenver22 <jeroenver22@users.noreply.github.com>
2021-07-28 17:04:38 +0200

    search function

--- js/index.js
+++ js/index.js
@@ -9,27 +9,33 @@ import { api } from "./utils.js";
 const api_url = "http://thecrew.cc/news/read.php";
 const post_url = "https://thecrew.cc/news/create.php";
 const main = document.getElementById("container");
+const $searchBar = document.getElementById("searchBar");
 
-const renderArticles = async () => {
+const renderArticles = async (query) => {
   main.innerHTML = "";
   //haalt articles op
   const articles = await api(api_url);
+  const searchByQuery = query ? articles.news.filter(({ title }) => title.toLowerCase().includes(query.toLowerCase())) : articles.news;
   //articles sorteren meeste naar minste sorteren
-  const sortedArticles = articles.news.sort((a, b) => b.likes - a.likes);
+  const sortedArticles = searchByQuery.sort((a, b) => b.likes - a.likes);
 
   //krijgt array terug met allemaal dom elementen
-  const rendered = sortedArticles.forEach(({ likes, title, content, UUID }) => {
-    // $ voor dom elementen aan te spreken
-    //zet ze in een nieuw element genaamd article
-    const $article = document.createElement("article");
-    $article.innerHTML = `
+  const rendered = sortedArticles.forEach(
+    ({ likes, title, content, UUID, imageURI, publicationDate }) => {
+      // $ voor dom elementen aan te spreken
+      //zet ze in een nieuw element genaamd article
+      const $article = document.createElement("article");
+      $article.innerHTML = `
         <h1>likes: ${likes}</h1>
         <h3>${title}</h3>
         <div>${content}</div>
+        ${imageURI != "" && `<img src="${imageURI}"/>`}
+        <p>${publicationDate}</p>
         <button data-like="${UUID}">Like article</button>
     `;
-    main.append($article);
-  });
+      main.append($article);
+    }
+  );
 };
 //voert functie uit
 renderArticles();
@@ -39,6 +45,10 @@ const likeArticle = async (UUID) => {
   const data = await response;
   if (data === "created") renderArticles();
 };
+//on keyup search in the object
+$searchBar.addEventListener("keyup", ({ target: { value } }) =>
+  renderArticles(value)
+);
 
 //event koppelen aan elk article button, checken of het wel dergelijk een button is, data set ophalen en mee geven in nieuwe functie
 main.addEventListener("click", ({ target }) => {
@@ -46,3 +56,5 @@ main.addEventListener("click", ({ target }) => {
   // if de islikebtn is clicked haal de dataset van de like btn op
   if (isLikeBtn) likeArticle(isLikeBtn.dataset.like);
 });
+
+const search = (query) => {};
