2d01558976341f35ef390d37d8ce93323f260805
jeroenver22 <jeroenver22@users.noreply.github.com>
2021-08-14 19:56:03 +0200

    updated index and sortby function
    i created a from element around the sort together with a better coded sort function

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 import { api } from "./utils.js";
 
+let sortLike = {userInput: {selectedSort:'likes'}};
 // http://thecrew.cc/news/read.php
 // https://thecrew.cc/news/create.php
 
@@ -10,21 +11,33 @@ const api_url = "http://thecrew.cc/news/read.php";
 const post_url = "https://thecrew.cc/news/create.php";
 const main = document.getElementById("container");
 const $searchBar = document.getElementById("searchBar");
+const $sortForm = document.getElementById('sortFn');
 
-const renderArticles = async (query) => {
+const sortDesc = (a, b) => b.likes - a.likes;
+const sortAsc = (a, b) => a.likes - b.likes;
+
+const sortArticles = (data, method='desc') => data.sort((a,b) => {
+  if(method ==='desc') return sortDesc(a,b);
+  if(method ==='asc') return sortAsc(a,b);
+
+}); 
+
+const renderArticles = async (query, sortMethod) => {
   main.innerHTML = "";
   //haalt articles op
+
+
   const articles = await api(api_url);
   console.log(articles);
-const searchByQuery = query ? articles.news.filter(({ title }) => title.toLowerCase().includes(query.toLowerCase())) : articles.news;  
+  const searchByQuery = query ? articles.news.filter(({ title }) => title.toLowerCase().includes(query.toLowerCase())) : articles.news;  
   //articles sorteren meeste naar minste sorteren
-  const sortedArticles = searchByQuery.sort((a, b) => b.likes - a.likes);
-/*   const sortedArticles = searchByQuery.forEach(function(radio){
-    const selectedSort = document.getElementsByName('sortBy');
-    selectedSort.addEventListener('change', function(e){
-      searchByQuery.userInput.selectedSort = this.likes;
-    });
-  }); */
+
+  const sortedArticles = sortArticles(searchByQuery, sortMethod);
+
+
+
+
+
   //krijgt array terug met allemaal dom elementen
   const rendered = sortedArticles.forEach(
     ({ likes, title, content, UUID, imageURI, publicationDate }) => {
@@ -61,18 +74,28 @@ main.addEventListener("click", ({ target }) => {
   // if de islikebtn is clicked haal de dataset van de like btn op
   if (isLikeBtn) likeArticle(isLikeBtn.dataset.like);
 });
+$sortForm.addEventListener('change', ({target}) => {
+  //data uit form halen
+  const f = target.closest('form');
 
-const search = (query) => {};
+
+  const formData = new FormData(f);
+  const value = formData.get('sortBy');
+  renderArticles( null, value);
+});
+
 
 
 //sort function
-const sortLikes = (likes) =>{
+/* const sortLikes = (likes) =>{
 const sortRadios = document.getElementsByName('sortBy');
-      sortRadios.forEach(function(radio) {
-        radio.addEventListener('change', function(event) {
-          likes.userInput.selectedSort = this.value;
-          likes.applyAll();
+      sortRadios.forEach(function(like) {
+        like.addEventListener('change', function(event) {
+          sortLike.userInput.selectedSort = this.value;
+          console.log(sortLike);
         });
       });
     };
-  sortLikes();
+  sortLikes(); */
+
+  //
